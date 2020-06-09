fc2c9d58695c3d200107389e365595bf452d9f68
zengweidaren <35059527+zengweidaren@users.noreply.github.com>
2020-06-09 21:38:16 +0800

    Update Demo1.java

--- src/com/zeng/collection/Demo1.java
+++ src/com/zeng/collection/Demo1.java
@@ -3,5 +3,6 @@ package com.zeng.collection;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("第一次上传GitHub");
+        System.out.println("第一次下载GitHub");
     }
 }
